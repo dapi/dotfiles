7addadab8dfff52b59b40ac66d74030f94b8eb9b
Danil Pismenny <email>
2015-03-20 20:36:36 +0100

    update mac-specific files

--- mac_install.sh
+++ mac_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-test -f .irbrc && ln -s dotfiles/irbrc .irbrc
-test -f .rdebugrc && ln -s dotfiles/rdebugrc .rdebugrc
-test -f .ackrc && ln -s dotfiles/ackrc .ackrc
-test -f .gitconfig && cp dotfiles/gitconfig .gitconfig
-test -f .gitignore_global && ln -s dotfiles/gitignore_global .gitignore_global
-#test -f .zshrc && ln -s dotfiles/zshrc.mac .zshrc
-# install oh-my-zsh and patch .zshrc
+test -f .irbrc || ln -s dotfiles/irbrc .irbrc
+test -f .vimrc || ln -s dotfiles/.vimrc .vimrc
+test -f .rdebugrc || ln -s dotfiles/rdebugrc .rdebugrc
+test -f .ackrc || ln -s dotfiles/ackrc .ackrc
+test -f .gitconfig || cp dotfiles/gitconfig .gitconfig
+test -f .gitignore_global || ln -s dotfiles/gitignore_global .gitignore_global
+test -f .zshrc || ln -s dotfiles/zshrc.mac .zshrc
+# echo install oh-my-zsh and patch .zshrc
