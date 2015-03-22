3924476f1885eeb99576627fcc6df93aca9026dd
Danil Pismenny <danil@orionet.ru>
2015-03-23 00:06:11 +0400

    install .ctags

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ test -f .rdebugrc || ln -s dotfiles/rdebugrc .rdebugrc
 test -f .ackrc || ln -s dotfiles/ackrc .ackrc
 test -f .gitconfig || cp dotfiles/gitconfig .gitconfig
 test -f .gitignore_global || ln -s dotfiles/gitignore_global .gitignore_global
+test -f .ctags || ln -s dotfiles/ctags .ctags
 
 test -f .zshrc && mv -f .zshrc .zshrc.before_dotfiles
 ln -s dotfiles/zshrc .zshrc
