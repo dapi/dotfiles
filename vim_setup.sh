4cc88ac4582c1961af72be7bad25f663f2f276b7
Danil Pismenny <danil@brandymint.ru>
2023-03-25 20:38:33 +0300

    Remove legacy .vimrc

--- vim_setup.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
-
-echo "Install VIM plugins"
-vim -R +PluginInstall +qall > /tmp/vim_setup.log
-echo "Install VIM plugins: done"
