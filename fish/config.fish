7067a4756ca29814bcb0c981e4625e644336c104
Danil Pismenny <danil@brandymint.ru>
2025-02-02 13:20:49 +0300

    Improve PAGER in fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set -g fish_greeting
+
+export PAGER=/opt/homebrew/bin/nvimpager
+export MANPAGER=/opt/homebrew/bin/nvimpager
+
+alias less=$PAGER
