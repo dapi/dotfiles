4957f1f2bbae39c6a45eccdd6c9605b8ca6633c4
Danil Pismenny <danil@brandymint.ru>
2025-02-02 16:27:28 +0300

    Change background color on ssh connection"

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,10 @@ set -g fish_greeting
 
 export PAGER=/opt/homebrew/bin/nvimpager
 export MANPAGER=/opt/homebrew/bin/nvimpager
+export TERM=xterm-256color
+export KUBECONFIG=~/.kube/config:~/.kube/ovh-kubeconfig.yml
 
 alias less=$PAGER
 alias office="ssh office.brandymint.ru"
+
+[ -n "$SSH_CONNECTION" ] && echo -e "\033]11;#1F2800\a"
