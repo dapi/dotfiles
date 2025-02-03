ca55b72fc2f79fc01228283437c8966e172da3a8
Danil Pismenny <danil@brandymint.ru>
2025-02-03 22:27:18 +0300

    Minor improve fonts.mk

--- makefiles/fonts.mk
+++ makefiles/fonts.mk
@@ -2,7 +2,8 @@ fonts: hack-nerd-font
 
 hack-nerd-font:
 	@echo "Install hack-nerd-font"
-	@ls -1 ~/Library/Fonts/*HackNerdFont* > /dev/null || \
+	@test -d /home/danil/Library/Fonts && \
+	ls -1 ~/Library/Fonts/*HackNerdFont* &2>1 /dev/null || \
 		(which brew && brew install font-hack-nerd-font || echo "No brew to install fonts")
 
 powerline-fonts:
