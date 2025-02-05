d9ab279c94f3c5fdd17adcb28e09e989a987483f
Danil Pismenny <danil@brandymint.ru>
2025-02-05 19:32:43 +0300

    Update fonts makefile

--- makefiles/fonts.mk
+++ makefiles/fonts.mk
@@ -3,7 +3,7 @@ fonts: hack-nerd-font
 hack-nerd-font:
 	@echo "Install hack-nerd-font"
 	@test -d /home/danil/Library/Fonts && \
-	ls -1 ~/Library/Fonts/*HackNerdFont* &2>1 /dev/null || \
+	ls -1 ~/Library/Fonts/*HackNerdFont* /dev/null || \
 		(which brew && brew install font-hack-nerd-font || echo "No brew to install fonts")
 
 powerline-fonts:
