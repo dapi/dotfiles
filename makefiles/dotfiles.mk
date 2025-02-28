208a4862a3b26a5db2de26c95659e47631d7e0d8
Danil Pismenny <danil@brandymint.ru>
2025-02-28 10:54:47 +0300

    Fix dofiles tasks

--- makefiles/dotfiles.mk
+++ makefiles/dotfiles.mk
@@ -1,8 +1,7 @@
-
 DOTFILES=~/.irbrc ~/.rdebugrc ~/.ackrc ~/.pryrc ~/.tmux.conf ~/.psqlrc ~/.gemrc ~/.ctags ~/.agignore ~/.gitconfig ~/.gitignore_global
 
 dotfiles: $(DOTFILES)
 
 .PHONY: $(DOTFILES)
 $(DOTFILES):
-	@$(MAKE) link-home-config FILE=$@
+	@$(MAKE) link-home-config DST=$@
