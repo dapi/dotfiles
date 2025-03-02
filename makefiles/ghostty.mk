d4cd67ba0d054a11ba669c8804b02e18cc0c725d
Danil Pismenny <danil@brandymint.ru>
2025-03-02 20:54:38 +0300

    Move ghostty to maco section

--- makefiles/ghostty.mk
+++
@@ -1,5 +0,0 @@
-DOTFILES:=${DOTFILES} ~/.config/ghostty
-PACKAGES:=$(PACKAGES) ghostty
-
-ghostty-copy-terminfo:
-	infocmp -x | ssh ${REMOTE_HOST} -- tic -x -
--- makefiles/macos.mk
+++ makefiles/macos.mk
@@ -1,9 +1,15 @@
 UNAME=$(shell uname)
+ifeq ($(UNAME),Darwin)
 APPLIES:=${APPLIES} macos-install
 
 macos-install:
-ifeq ($(UNAME),Darwin)
 	@echo "Check Rosetta"
 	@arch -x86_64 /usr/bin/true || \
 		softwareupdate --install-rosetta --agree-to-license
+
+DOTFILES:=${DOTFILES} ~/.config/ghostty
+PACKAGES:=$(PACKAGES) ghostty
+
+ghostty-copy-terminfo:
+	infocmp -x | ssh ${REMOTE_HOST} -- tic -x -
 endif
