6170fe1621bdfbb30a7e9410a0c5711701a30cf9
Danil Pismenny <danil@brandymint.ru>
2025-11-11 19:35:06 +0300

    Add zai

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,16 @@ else
     exit 0
 end
 
+function zai
+    env ANTHROPIC_AUTH_TOKEN=$(pass show zai) \
+        ANTHROPIC_BASE_URL="https://api.z.ai/api/anthropic" \
+        ANTHROPIC_DEFAULT_HAIKU_MODEL="glm-4.5-air" \
+        ANTHROPIC_DEFAULT_SONNET_MODEL="glm-4.6" \
+        ANTHROPIC_DEFAULT_OPUS_MODEL="glm-4.6" \
+        ANTHROPIC_MODEL="glm-4.6" \
+        claude $argv
+end
+
 # Add basic paths if not already present
 #fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 
