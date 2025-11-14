fe1bedbac84fa4c290b10814e94c156cfc3f1195
Danil Pismenny <dapi@users.noreply.github.com>
2025-11-14 17:18:06 +0300

    Update fish/config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,7 +21,7 @@ function zai
         claude $argv
 end
 
-function kimi
+function kimi-claude
 	env ANTHROPIC_AUTH_TOKEN=$(pass show kimi) \
 	ANTHROPIC_BASE_URL=https://api.moonshot.ai/anthropic \
 	ANTHROPIC_MODEL=kimi-k2-thinking \
