51f02dc4725cc367ffb1577d9815569a6b3b89ff
Danil Pismenny <dapi@users.noreply.github.com>
2025-11-19 21:06:07 +0300

    Fix proxy zai

--- .config/fish/functions/proxy.fish
+++ .config/fish/functions/proxy.fish
@@ -1,3 +1,5 @@
-function proxy
-    env http_proxy=http://(pass show proxy/current) HTTPS_PROXY=http://(pass show proxy/current) $argv
+function proxy --description 'Run command with proxy settings'
+    set -gx http_proxy "http://"(pass show proxy/current)
+    set -gx HTTPS_PROXY "http://"(pass show proxy/current)
+    $argv
 end
