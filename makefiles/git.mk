4da3b8ee18766c9e513ca03becf745377a733296
Danil Pismenny <dapi@users.noreply.github.com>
2026-01-30 11:28:07 +0000

    Improve dotfile for ofifice2

--- makefiles/git.mk
+++ makefiles/git.mk
@@ -1,4 +1,4 @@
-PACKAGES:=$(PACKAGES) git
+PACKAGES:=$(PACKAGES) git git-crypt
 DOTFILES:=$(DOTFILES) ~/.gitconfig ~/.gitignore_global 
 
 git-pull:
--- makefiles/packages.mk
+++ makefiles/packages.mk
@@ -1,3 +1,5 @@
+PACKAGES:=$(PACKAGES) mise
+
 package:
 	@echo "Install ${PACKAGE}"
 	@which ${PACKAGE} > /dev/null || (which brew > /dev/null && brew install ${PACKAGE} || sudo apt-get install ${PACKAGE})
