ca91e5ee8e4e018f8443dee70f943e7e5e2ff7dd
Danil Pismenny <dapi@users.noreply.github.com>
2026-03-06 12:12:21 +0300

    Rename fish function ssh to sshr

--- .config/fish/functions/ssh.fish
+++ .config/fish/functions/ssh.fish
@@ -1,4 +1,4 @@
-function ssh --wraps=ssh --description "SSH with Ghostty theme change"
+function sshr --wraps=ssh --description "SSH with Ghostty theme change"
     switch "$argv"
         case '*office2*'
             # Office2 - dark blue theme
