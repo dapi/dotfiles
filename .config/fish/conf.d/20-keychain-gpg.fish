b67b5f4b534642e4aedf9344de38eb636eae710f
Danil Pismenny <dapi@users.noreply.github.com>
2026-03-12 15:46:25 +0300

    Prefer forwarded GPG agent over a locally spawned one during SSH sessions

--- .config/fish/conf.d/20-keychain-gpg.fish
+++ .config/fish/conf.d/20-keychain-gpg.fish
@@ -5,5 +5,10 @@ if test (uname) = Linux
     set -gx SSH_AUTH_SOCK /run/user/(id -u)/openssh_agent
 end
 
+# Prefer forwarded GPG agent over a locally spawned one during SSH sessions.
+if set -q SSH_CONNECTION
+    gpgconf --kill gpg-agent >/dev/null 2>&1
+end
+
 # Tell pinentry which terminal to use.
 set -gx GPG_TTY (tty)
