550b93cf16668f89457314c843b6900bb9e2d535
Danil Pismenny <dapi@users.noreply.github.com>
2026-03-12 15:46:11 +0300

    Update lazyload BUGSNAG_DATA_API_KEY

--- .config/fish/conf.d/30-proxy-lazy.fish
+++ .config/fish/conf.d/30-proxy-lazy.fish
@@ -19,3 +19,21 @@ function __load_bugsnag_proxy_once --description 'Load BUGSNAG_HTTP_PROXY from p
         set -gx BUGSNAG_HTTP_PROXY $bugsnag_proxy
     end
 end
+
+# Load BUGSNAG_DATA_API_KEY explicitly on demand.
+function load_bugsnag_data_api_key --description 'Load BUGSNAG_DATA_API_KEY from pass'
+    if test (uname) != "Linux"
+        return 1
+    end
+    if not type -q pass
+        return 1
+    end
+
+    set -l bugsnag_data_api_key (pass show bugsnag/brandymint-api-key 2>/dev/null)
+    if test -n "$bugsnag_data_api_key"
+        set -gx BUGSNAG_DATA_API_KEY $bugsnag_data_api_key
+        return 0
+    end
+
+    return 1
+end
