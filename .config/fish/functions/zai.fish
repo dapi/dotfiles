91727d8431fd152669f1cd72c46cf73e77318b35
Danil Pismenny <dapi@users.noreply.github.com>
2026-04-20 01:48:42 +0800

    Update zai

--- .config/fish/functions/zai.fish
+++ .config/fish/functions/zai.fish
@@ -1,9 +1,8 @@
 function zai --description 'Run claude with ZAI configuration'
     env ANTHROPIC_AUTH_TOKEN=(pass show zai) \
         ANTHROPIC_BASE_URL="https://api.z.ai/api/anthropic" \
-        # ANTHROPIC_DEFAULT_HAIKU_MODEL="glm-4.5-air" \
-        ANTHROPIC_DEFAULT_HAIKU_MODEL="glm-5-turbo" \
-        ANTHROPIC_DEFAULT_SONNET_MODEL="glm-4.7" \
+        ANTHROPIC_DEFAULT_HAIKU_MODEL="glm-4.5-air" \
+        ANTHROPIC_DEFAULT_SONNET_MODEL="glm-5-turbo" \
         ANTHROPIC_DEFAULT_OPUS_MODEL="glm-5.1" \
         ANTHROPIC_MODEL="glm-5.1" \
         API_TIMEOUT_MS="3000000" \
